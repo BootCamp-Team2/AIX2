b006827cb3db2a32b10cf14bb5b545f7af7db106
koye0201 <koye0201@naver.com>
2024-12-31 17:47:56 +0900

    Chore : 메인화면, 탭바 수정 미완

--- love-project/src/screens/MainScreen.js
+++ love-project/src/screens/MainScreen.js
@@ -20,14 +20,6 @@ const MainScreen = () => {
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("CoordinationScreen")}>
         <Text style={styles.buttonText}>코디 화면</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("ProfileScreen")}>
-        <Text style={styles.buttonText}>프로필 화면</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("SettingScreen")}>
-        <Text style={styles.buttonText}>설정 화면</Text>
-      </TouchableOpacity>
     </View>
   );
 };
@@ -36,7 +28,7 @@ const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
   button: {
-    backgroundColor: '#009688',
+    backgroundColor: '#FF9AAB',
     padding: 15,
     borderRadius: 8,
     width: '100%',
