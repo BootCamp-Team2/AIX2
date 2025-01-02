51b4b5bdbed32f5902ddf38549283c356e4883a3
koye0201 <koye0201@naver.com>
2025-01-02 19:33:24 +0900

    Chore : 프로필 화면 수정 구현 및 디자인 수정

--- love-project/src/screens/Profile/ProfileScreen.js
+++ love-project/src/screens/Profile/ProfileScreen.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image, Alert, TextInput, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
+import { MaterialIcons } from '@expo/vector-icons'; // Expo Icons 추가
+import Icon2 from 'react-native-vector-icons/Feather';
+import Icon3 from 'react-native-vector-icons/Ionicons'; 
+import Icon4 from 'react-native-vector-icons/FontAwesome5';
+import Icon5 from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const ProfileScreen = () => {
     const navigation = useNavigation();
@@ -22,7 +27,6 @@ const ProfileScreen = () => {
         if (newInfo.title && newInfo.value) {
             setAdditionalInfo([...additionalInfo, { title: newInfo.title, value: newInfo.value }]);
             setNewInfo({ title: '', value: '' }); // 입력 필드 초기화
-            setShowInput(false); // 입력 필드 숨기기
         } else {
             Alert.alert("오류", "모든 필드를 입력해주세요.");
         }
@@ -44,8 +48,11 @@ const ProfileScreen = () => {
             >
                 <View style={styles.header}>
                     <Text style={styles.headerText}>홍길동 님의 프로필</Text>
-                    <TouchableOpacity style={styles.editButton} onPress={() => { setShowInput(!showInput); setShowEditButtons(!showEditButtons); }}>
-                        <Text style={styles.editButtonText}>✏️</Text>
+                    <TouchableOpacity style={styles.editButton} onPress={() => {
+                        setShowInput(!showEditButtons);
+                        setShowEditButtons(!showEditButtons);
+                    }}>
+                        <Icon2 style={styles.editButtonText} name="edit-3" size={24} color="#9AAEFF" />
                     </TouchableOpacity>
                 </View>
 
@@ -63,6 +70,7 @@ const ProfileScreen = () => {
                     >
                         <Text style={[styles.segmentText, selectedIndex === 0 ? styles.activeText : styles.inactiveText]}>정보</Text>
                     </TouchableOpacity>
+
                     <TouchableOpacity
                         style={[styles.segment, selectedIndex === 1 ? styles.activeSegment : styles.inactiveSegment]}
                         onPress={() => setSelectedIndex(1)}
@@ -70,7 +78,8 @@ const ProfileScreen = () => {
                         <Text style={[styles.segmentText, selectedIndex === 1 ? styles.activeText : styles.inactiveText]}>미디어</Text>
                     </TouchableOpacity>
                 </View>
-
+                
+                {selectedIndex === 0 ? (
                 <View style={styles.infoContainer}>
                     {/* 상단 정보 배치: MBTI, 나이, 지역을 가로로 배치 */}
                     <View style={styles.infoRow}>
@@ -127,17 +136,24 @@ const ProfileScreen = () => {
                                     <Text style={styles.editButtonText}>수정</Text>
                                 </TouchableOpacity>
                             )}
-                            <TouchableOpacity onPress={() => handleDeleteInfo(index)} style={styles.deleteButton}>
-                                <Text style={styles.deleteButtonText}>삭제</Text>
-                            </TouchableOpacity>
+                            {showEditButtons && (
+                                <TouchableOpacity onPress={() => handleDeleteInfo(index)} style={styles.deleteButton}>
+                                    <Text style={styles.deleteButtonText}>삭제</Text>
+                                </TouchableOpacity>
+                            )}
                         </View>
                     ))}
                 </View>
+                ) : (
+                    <View style={styles.mediaContainer}>
+                        <Text style={styles.mediaText}>여기에 사진 업로드 UI를 추가하세요.</Text>
+                    </View>
+                )}
 
             </ScrollView>
 
             {/* 새 정보 추가 입력 필드 */}
-            {showInput && (
+            {showEditButtons && (
                 <View style={styles.inputContainer}>
                     <TextInput
                         style={styles.input}
@@ -156,6 +172,10 @@ const ProfileScreen = () => {
                     </TouchableOpacity>
                 </View>
             )}
+
+            <View style={styles.appeal}>
+                <Text style={styles.appealText}>어필하고 싶은 내용을 적어보세요!</Text>
+            </View>
         </KeyboardAvoidingView>
     );
 };
@@ -165,7 +185,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-start',
         padding: 16,
-        backgroundColor: '#F9F9F9', // 배경 색상
+        backgroundColor: '#F9F9F9',
     },
     header: {
         marginBottom: 20,
@@ -178,7 +198,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 50,
         marginLeft: 85,
-        color: '#333', // 텍스트 색상
+        color: '#9AAEFF',
     },
     editButton: {
         marginLeft: 10,
@@ -196,45 +216,45 @@ const styles = StyleSheet.create({
         height: 100,
         borderRadius: 50,
         borderWidth: 2,
-        borderColor: '#E0E0E0', // 사진 테두리 색상
+        borderColor: '#E0E0E0',
     },
     segmentedControl: {
         flexDirection: 'row',
-        backgroundColor: '#FFFFFF', // 배경 색상
+        backgroundColor: '#FFFFFF',
         borderRadius: 5,
         marginBottom: 20,
-        shadowColor: '#000', // 그림자 색상
+        shadowColor: '#000',
         shadowOffset: {
             width: 0,
             height: 2,
         },
         shadowOpacity: 0.1,
         shadowRadius: 4,
-        elevation: 2, // 안드로이드에서 그림자 효과
-        overflow: 'hidden', // 둥글게 만들기 위해 추가
+        elevation: 2,
+        overflow: 'hidden',
     },
     segment: {
         flex: 1,
         paddingVertical: 15,
         alignItems: 'center',
         justifyContent: 'center',
-        borderRadius: 5, // 둥글게 만들기
+        borderRadius: 5,
     },
     activeSegment: {
-        backgroundColor: '#FFFFFF', // 선택된 버튼의 배경색 (하얀색)
+        backgroundColor: '#FFFFFF',
     },
     inactiveSegment: {
-        backgroundColor: '#E0E0E0', // 눌리지 않은 버튼의 배경색 (회색)
+        backgroundColor: '#E0E0E0',
     },
     segmentText: {
         fontWeight: '500',
     },
     activeText: {
-        color: '#333', // 선택된 버튼의 텍스트 색상
+        color: '#333',
         fontWeight: 'bold',
     },
     inactiveText: {
-        color: '#666', // 눌리지 않은 버튼의 텍스트 색상 (회색)
+        color: '#666',
     },
     infoContainer: {
         marginTop: 20,
@@ -257,14 +277,14 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.1,
         shadowRadius: 4,
-        elevation: 2, // 안드로이드에서 그림자 효과
-        flex: 1, // 박스가 동일한 너비를 가지도록 설정
-        marginHorizontal: 5, // 박스 간격
+        elevation: 2,
+        flex: 1,
+        marginHorizontal: 5,
     },
     infoText: {
         fontSize: 16,
-        color: '#444', // 정보 텍스트 색상
-        textAlign: 'center', // 텍스트 중앙 정렬
+        color: '#444',
+        textAlign: 'center',
     },
     hobbyBox: {
         backgroundColor: '#FFFFFF',
@@ -279,12 +299,12 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.1,
         shadowRadius: 4,
-        elevation: 2, // 안드로이드에서 그림자 효과
-        marginBottom: 10, // 박스 간격
+        elevation: 2,
+        marginBottom: 10,
     },
     hobbyText: {
         fontSize: 16,
-        color: '#444', // 취미 및 동물 텍스트 색상
+        color: '#444',
     },
     inputContainer: {
         flexDirection: 'row',
@@ -297,7 +317,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 10,
         flex: 1,
-        marginRight: 10, // 입력 필드 간 간격
+        marginRight: 10,
     },
     addButton: {
         backgroundColor: '#FF6F61',
@@ -320,6 +340,13 @@ const styles = StyleSheet.create({
     deleteButtonText: {
         color: '#FFFFFF',
         fontWeight: 'bold',
+    },
+    appeal: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    appealText: {
+        color: '#9AAEFF',
     },
 });
 
