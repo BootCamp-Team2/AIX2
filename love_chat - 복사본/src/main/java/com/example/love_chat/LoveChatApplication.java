f95b796e60ce0877a193a1e22fb97a0bce24b1c2
kimbyeungjo <dana1197@naver.com>
2025-01-03 15:17:15 +0900

    chat

---
+++ love_chat - 복사본/src/main/java/com/example/love_chat/LoveChatApplication.java
@@ -0,0 +1,15 @@
+package com.example.love_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class LoveChatApplication {
+
+	public static void main(String[] args) {
+		System.out.println("start!");
+		SpringApplication.run(LoveChatApplication.class, args);
+	}
+
+}
