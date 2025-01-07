6da3db53731fdd20a7e118e0e9ae734592b7fd92
KimShun <kimsih0818@gmail.com>
2025-01-07 15:58:04 +0900

    Chore: feature 추가 및 코드 수정

--- Content_Based_Filtering/README.md
+++ Content_Based_Filtering/README.md
@@ -17,3 +17,27 @@
 - MBTI 뿐만 아니라 에니어그램을 통해서도 상대를 추천해주는 것이 어떨까...?!
     - MBTI만으로 추천하기에는 너무 적다.
     - MBTI와 에니어그램의 성향이 겹치지는 않을까?!
+
+#### 2025-01-07
+- MBTI 말고도 다른 요소 추가하여 추천의 폭을 넓혔습니다.
+    - 추가한 항목 : 
+        - 성별(남성/여성)
+        - 신장(작음/평균/큼)
+        - 외모(귀여움, 매력적, 활기참, 미소, 단아함, 청순함, 중성미, 카리스마, 스포티, 패션감각) [최대 3개 중복가능]
+            - 유저가 선호하는 외모 선택할때, "상관없음" 할 시에 전원 체크!! ( 따로 상관없음 만들지 않겠습니다. )
+    - mySQL 데이터베이스 테이블 구조변경하고 테스트 진행했습니다. ( 테이블이름: idealTable )
+        - 테이블 구조 : 
+            - userUID(char): 각 유저가 가지는 고유번호
+            - myGender(varchar): 유저 성별
+            - recommendGender(varchar): 추천하는 성별
+            - myMBTI(varchar): 유저 MBTI
+            - recommendMBTI(JSON): 추천하는 MBTI
+            - myHeight(varchar): 유저 신장
+            - favoriteHeight(varchar): 선호하는 신장
+            - myAppearance(JSON): 유저 외모
+            - favoriteAppearance(JSON): 선호하는 외모
+    - faiss_main.py & database.py 파일 수정했습니다.
+        - faiss_main.py : 사용자에게 적합한 상대 추천
+        - database.py : MySQL 서버의 idealType 테이블과 연동하여 fastAPI로 사용자의 데이터를 추가 및 수정
+    - 테스트 데이터 100개 
+        - idealType_100_***.sql 파일 참고바람
--- Content_Based_Filtering/faiss_main.py
+++ Content_Based_Filtering/faiss_main.py
@@ -4,36 +4,45 @@ import faiss
 import numpy as np
 import json
 
-# 원핫인코딩 함수
+## 원핫인코딩 리스트
+gender_types = ["남성", "여성"]
+
 mbti_types = [
     "INTJ", "INFP", "ENTP", "ESFP", "ISTJ", "ENFJ", "INFJ", "ISFP",
     "ENTJ", "ESFJ", "INTP", "ESTP", "ISFJ", "ESTJ", "ENFP", "INFE"
 ]
 
-def mbti_to_onehot(mbti, mbti_types=mbti_types):
-    vector = [1 if mbti == t else 0 for t in mbti_types]
+height_types = ["작음", "평균", "큼"]
+
+appearance_types = [
+    "귀여움", "매력적", "활기참", "미소", "단아함", 
+    "청순함", "중성미", "카리스마", "스포티", "패션감각"
+]
+
+def to_one_hot(value, value_types):
+    vector = [1 if value == t else 0 for t in value_types]
     return vector
 
-def mbti_list_to_one_hot(mbti_list, mbti_types=mbti_types):
-    combined_vector = np.zeros(len(mbti_types), dtype=int) # 빈 벡터 생성 - 전부 0
-    for mbti in mbti_list:
-        single_vector = mbti_to_onehot(mbti, mbti_types)
+def list_to_multiply_hot(value_list, value_types):
+    combined_vector = np.zeros(len(value_types), dtype=int) # 빈 벡터 생성 - 전부 0
+    for value in value_list:
+        single_vector = to_one_hot(value, value_types)
         combined_vector = np.maximum(combined_vector, single_vector)  # 합집합 처리
     return combined_vector.tolist()
 
 def fetch_mbti_data(userUID):
-    # MySQL Server 연결
+    ## MySQL Server 연결
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
         password="AIX2_1234",
-        database="myMBTIDatabase"
+        database="idealDatabase"
     )
     
     cursor = conn.cursor()
     
-    # 해당 유저의 행 뽑아오기
-    query_row = "SELECT * FROM mbtiTable WHERE userUID = %s"
+    ## 해당 유저의 행 뽑아오기
+    query_row = "SELECT * FROM idealTable WHERE userUID = %s"
     cursor.execute(query_row, (userUID, ))
     results_user = cursor.fetchall()
     
@@ -43,60 +52,83 @@ def fetch_mbti_data(userUID):
     # for row in results_user:
     #     print(row)
     
-    query_row = "SELECT * FROM mbtiTable WHERE userUID != %s"
+    query_row = "SELECT * FROM idealTable WHERE userUID != %s"
     cursor.execute(query_row, (userUID, ))
     results_others = cursor.fetchall()
         
     # for row in results_others:
     #     print(row)
-
+    
+    user_df = pd.DataFrame(results_user, columns=["userUID", "myGender", "recommendGender", "myMBTI", "recommendMBTI", 
+                                                  "myHeight", "favoriteHeight", "myAppearance", "favoriteAppearance"])
+    others_df = pd.DataFrame(results_others, columns=["userUID", "myGender", "recommendGender", "myMBTI", "recommendMBTI", 
+                                                  "myHeight", "favoriteHeight", "myAppearance", "favoriteAppearance"])
+    
+    ## 타입이 JSON인 feature 이름 가져오기
+    query_row = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = %s AND DATA_TYPE = 'JSON'"
+    cursor.execute(query_row, ("idealTable", ))
+    json_columns = [column[0] for column in cursor.fetchall()]
+    
+    print(json_columns)
+    
     cursor.close()
     conn.close()
-    
-    user_df = pd.DataFrame(results_user, columns=["userUID", "myMBTI", "recommendMBTI"])
-    others_df = pd.DataFrame(results_others, columns=["userUID", "myMBTI", "recommendMBTI"])
-    
-    return user_df, others_df
+    return user_df, others_df, json_columns
 
 def process_data(userUID):
-    user_df, others_df = fetch_mbti_data(userUID)
-    # print(user_df.head()); print(others_df.head())
-
-    user_df["recommendMBTI"] = user_df["recommendMBTI"].apply(lambda x: json.loads(x))
-    others_df["recommendMBTI"] = others_df["recommendMBTI"].apply(lambda x: json.loads(x))
+    user_df, others_df, json_columns = fetch_mbti_data(userUID)
     # print(user_df.head()); print(others_df.head())
     
-    user_df["myMBTI"] = user_df["myMBTI"].apply(mbti_to_onehot)
-    user_df["recommendMBTI"] = user_df["recommendMBTI"].apply(mbti_list_to_one_hot)
-
-    others_df["myMBTI"] = others_df["myMBTI"].apply(mbti_to_onehot)
-    others_df["recommendMBTI"] = others_df["recommendMBTI"].apply(mbti_list_to_one_hot)
+    for feature in json_columns:
+        user_df[feature] = user_df[feature].apply(lambda x: json.loads(x))
+        others_df[feature] = others_df[feature].apply(lambda x: json.loads(x))
+    # print(user_df.head()); print(others_df.head())
+    
+    feature_list = user_df.columns[user_df.columns != "userUID"]
+    type_list = [gender_types, mbti_types, height_types, appearance_types]
+    for i, feature in enumerate(feature_list):
+        if not isinstance(user_df[feature][0], list):
+            user_df[feature] = user_df[feature].apply(lambda x: to_one_hot(x, type_list[i // 2]))
+            others_df[feature] = others_df[feature].apply(lambda x: to_one_hot(x, type_list[i // 2]))
+        else:
+            user_df[feature] = user_df[feature].apply(lambda x: list_to_multiply_hot(x, type_list[i // 2]))
+            others_df[feature] = others_df[feature].apply(lambda x: list_to_multiply_hot(x, type_list[i // 2]))
     # print(user_df.head()); print(others_df.head())
 
-    # FAISS를 위한 벡터리스트로 변환
-    user_recommendMBTI_vectors = np.array(user_df["recommendMBTI"].tolist(), dtype=np.float32)
-    others_myMBTI_vectors = np.array(others_df["myMBTI"].tolist(), dtype=np.float32)
-
-    # 벡터 정규화
-    faiss.normalize_L2(user_recommendMBTI_vectors)
-    faiss.normalize_L2(others_myMBTI_vectors)
-
-    # FAISS 인덱스 생성
-    index = faiss.IndexFlatL2(user_recommendMBTI_vectors.shape[1])
-    index.add(others_myMBTI_vectors)
+    ## FAISS를 위한 벡터리스트로 변환
+    user_vector_list = [feature for i, feature in enumerate(feature_list) if i % 2 != 0]
+    # print(f"user_vector: {user_vector_list}")
+    others_vector_list = [feature for i, feature in enumerate(feature_list) if i % 2 == 0]
+    # print(f"other_vector: {others_vector_list}")
     
-    # 유사도 측정 - 최대 5개
-    distances, indices = index.search(user_recommendMBTI_vectors, k=5)#others_myMBTI_vectors, k=5)
-    # similar_users = [others_myMBTI_vectors[i] for i in indices[0]]
+    ## 벡터 결합
+    user_combined_vectors = np.array([np.concatenate([user_df[col][i] for col in user_vector_list], axis=None) for i in range(len(user_df))], dtype=np.float32)
+    others_combined_vectors = np.array([np.concatenate([others_df[col][i] for col in others_vector_list], axis=None) for i in range(len(others_df))], dtype=np.float32)
+    
+    ## 벡터 정규화
+    faiss.normalize_L2(user_combined_vectors)
+    faiss.normalize_L2(others_combined_vectors)
+
+    ## FAISS 인덱스 생성
+    index = faiss.IndexFlatL2(user_combined_vectors.shape[1])
+    index.add(others_combined_vectors)
+    
+    ## 유사도 측정 - 최대 5개
+    distances, indices = index.search(user_combined_vectors, k=5)#others_myMBTI_vectors, k=5)
+    # similar_users = [others_combined_vectors[i] for i in indices[0]]
     # print("유사한 벡터:", similar_users)
+    
+    #### 주의!! 회원 수가 적어서 추천해줄 수 있는 인원이 적은 경우 indices 값에 "-1" 들어있을 수 있습니다.
+    valid_indices = [i for i in indices[0] if i != -1]
 
-    # 최종결과 - 인덱스로 userUID 매핑
-    similar_userUIDs = [others_df["userUID"][i] for i in indices[0]]
+    ## 최종결과 - 인덱스로 userUID 매핑
+    similar_userUIDs = [others_df["userUID"][i] for i in valid_indices]
     # print("유사한 userUIDs:", similar_userUIDs)
     
     return similar_userUIDs
+    # return None
 
-# 임시로 유저UID 다음과 같이 지정
+## 임시로 유저UID 다음과 같이 지정
 def main(userUID):
     try:
         recommend_result = process_data(userUID)
@@ -105,5 +137,5 @@ def main(userUID):
         print(f"오류: {e}")
     
 if __name__ == "__main__":
-    main("0069994997")
-    # main("김시현") # 존재하지 않는 userUID 테스트
+    main("0175425703")
+    # main("김시현") ## 존재하지 않는 userUID 테스트
