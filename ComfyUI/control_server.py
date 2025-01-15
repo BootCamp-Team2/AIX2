35bebd869ec9c682806150dc631250fd22531121
KimShun <kimsih0818@gmail.com>
2025-01-15 10:05:55 +0900

    Chore: ComfyUI - fastAPI 코드 수정

--- ComfyUI/control_server.py
+++ ComfyUI/control_server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -21,9 +21,14 @@ app.mount("/input", StaticFiles(directory=INPUT_FOLDER), name="input")
 app.mount("/output", StaticFiles(directory=OUTPUT_FOLDER), name="output")
 
 ## false - 사용 가능 / true - 사용 불가능
-server_status: Dict[str, bool] = {
-    "http://192.168.1.2:8001": False,
-    "http://192.168.1.2:8004": False,
+avatar_server_status: Dict[str, bool] = {
+    "http://192.168.1.4:8001": False,
+    "http://192.168.1.4:8002": False,
+}
+
+sim_server_status: Dict[str, bool] = {
+    "http://192.168.1.4:9001": False,
+    "http://192.168.1.4:9001": False,
 }
 
 class ServerStatus(BaseModel):
@@ -32,34 +37,33 @@ class ServerStatus(BaseModel):
 
 @app.get("/check-server")
 async def check_server(server_ip: str):
-    status = server_status.get(server_ip, None)
+    status = avatar_server_status.get(server_ip, None)
     if status is None:
         return {"error": "Server not found"}
     return {"server_name": server_ip, "status": status}
 
 @app.post("/update-status")
 async def update_status(status: ServerStatus):    
-    """
-    각 FastAPI 서버가 상태를 업데이트하는 요청을 보냅니다.
-    """
-    server_status[status.server_ip] = status.status
+    avatar_server_status[status.server_ip] = status.status
     return {"message": f"Server {status.server_ip} status updated to {status.status}"}
 
 
-@app.get("/select-server")
-async def select_server():
-    """
-    사용 가능한 FastAPI 서버를 선택하여 응답합니다. (사용 가능한 서버가 없다면 다른 처리)
-    """
-    available_servers = [server for server, status in server_status.items() if not status]
+@app.post("/select-server")
+async def select_server(type: str = Form(...)):
+    server_status = avatar_server_status.items() if type == "avatar" else sim_server_status.items()
+    available_servers = [server for server, status in server_status if not status]
     
     if available_servers:
         selected_server = available_servers[0]  # 첫 번째로 사용 가능한 서버
-        server_status[selected_server] = True  # 서버 사용 중으로 변경
+        if type == "avatar":
+            avatar_server_status[selected_server] = True  # 서버 사용 중으로 변경
+        elif type == "sim":
+            sim_server_status[selected_server] = True
+        
         return {"server_ip": f"{selected_server}"}
     else:
         return {"server_ip": ""}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("control_server:app", host="192.168.1.2", port=1000)
+    uvicorn.run("control_server:app", host="192.168.1.4", port=1000)
--- ComfyUI/sim_model_run.py
+++ ComfyUI/sim_model_run.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form, HTTPException, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -6,11 +6,26 @@ import importlib.util
 import os
 import uuid
 import shutil
+import httpx
 
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 app = FastAPI()
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = "8000"
+
+@app.middleware("http")
+async def detect_server_info(request: Request, call_next):
+    global SERVER_HOST, SERVER_PORT
+    server = request.scope.get("server", None)
+    if server:
+        SERVER_HOST = server[0]
+        SERVER_PORT = server[1]
+    response = await call_next(request)
+    return response
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://192.168.1.30:8081"],  # React 개발 서버 주소
@@ -33,6 +48,10 @@ spec = importlib.util.spec_from_file_location(module_name, module_path)
 idealType_module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(idealType_module)
     
+@app.get("/get/ip")
+def getServerIP():
+    return {"server-ip": f"{SERVER_HOST}", "server-port": f"{SERVER_PORT}"}
+
 @app.post("/sim/create")
 async def createMyLover(ideal_type: str = Form(...)):
     # 고객 uuid 대체용 임시 테스트
@@ -55,16 +74,31 @@ async def createMyLover(ideal_type: str = Form(...)):
         new_file_path = os.path.join(new_file_director, "mySimulator.jpg")
         
         shutil.move(result_path, new_file_path)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                "http://192.168.1.4:1000/update-status", 
+                json={"server_ip": f"http://{SERVER_HOST}:{SERVER_PORT}", "status": False}
+            )
+            if response.status_code != 200:
+                raise HTTPException(status_code=response.status_code, detail=f"Error: {response.text}")
         
     except Exception as e:
         return JSONResponse({"error": str(e)}, status_code=500)
     
+    finally:
+        # 모델 실행 후 메모리 해제
+        import gc
+        gc.collect()  # Python에서 객체들의 메모리를 수동으로 회수
+        
+        import torch
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()  # GPU 메모리 캐시 정리
     
     return {
         "message": "Your Requested prompt successfully",
-        "simUrl": f"http://192.168.1.2:1000/output/{personal_uuid}/mySimulator.jpg"
+        "simUrl": f"http://192.168.1.4:1000/output/{personal_uuid}/mySimulator.jpg"
     }
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("sim_model_run:app", host="192.168.1.2", port=8000)
+    uvicorn.run("avatar_model_run:app", host="0.0.0.0", port=8000)
