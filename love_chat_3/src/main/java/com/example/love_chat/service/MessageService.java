0abd8145b8b1164130db100e2c0e9cb0cbc86159
KimShun <sihyun@sihyuns-MacBook-Air.local>
2025-01-21 11:36:14 +0900

    Chore: MySql 테이블 생성 변경

--- love_chat_3/src/main/java/com/example/love_chat/service/MessageService.java
+++ love_chat_3/src/main/java/com/example/love_chat/service/MessageService.java
@@ -1,6 +1,6 @@
 package com.example.love_chat.service;
 
-import com.example.love_chat.model.Message;
+import com.example.love_chat.model.UserChatRecord;
 import com.example.love_chat.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,16 +13,16 @@ public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
 
-    public Message saveMessage(Message message) {
+    public UserChatRecord saveMessage(UserChatRecord message) {
         message.setTimestamp(java.time.LocalDateTime.now()); // 타임스탬프 설정
         return messageRepository.save(message); // 메시지 저장
     }
 
-    public List<Message> getMessagesForUser(String username) {
+    public List<UserChatRecord> getMessagesForUser(String username) {
         return messageRepository.findByReceiver(username); // 특정 사용자의 메시지 조회
     }
 
-    public List<Message> getMessagesBetweenUsers(String sender, String receiver) {
+    public List<UserChatRecord> getMessagesBetweenUsers(String sender, String receiver) {
         return messageRepository.findBySenderAndReceiver(sender, receiver); // 두 사용자 간 메시지 조회
     }
 }
