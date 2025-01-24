7b8ea69bd1ff825cd54ef4abb6ebad83c6ad9451
koye0201 <koye0201@naver.com>
2025-01-24 17:58:21 +0900

    Chore: 아바타 생성 화면 오류 수정 취소

--- love-project/src/screens/Profile/AvatarScreen.js
+++ love-project/src/screens/Profile/AvatarScreen.js
@@ -9,7 +9,8 @@ import { Ionicons } from '@expo/vector-icons';
 
 const AvatarScreen = () => {
   const route = useRoute();
-  const { userUID } = route.params  || { userUID: null }; // 기본값 설정
+  // const { userUID } = route.params  || { userUID: null }; // 기본값 설정
+  const { userUID } = route.params
 
   const navigation = useNavigation();
   const [imageUri, setImageUri] = useState(null);
